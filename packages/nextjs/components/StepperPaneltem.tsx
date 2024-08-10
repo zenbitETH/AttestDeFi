d67dd61ba912e2b034ddae02cc1b1617cc4002f5
habacuc.eth <habacuc.vera@gmail.com>
2024-08-10 12:39:52 -0600

    Panel and buttons

--- packages/nextjs/components/StepperPaneltem.tsx
+++ packages/nextjs/components/StepperPaneltem.tsx
@@ -21,13 +21,11 @@ export const StepperPaneltem = ({ step, type, on }: NavItemType) => {
   return (
     <motion.div variants={navItemVariant} className="flex md:p-3 md:items-center md:py-0 gap-3">
       <div
-        className={`h-10 w-10 border font-bold border-white grid place-items-center rounded-full transition duration-500 ease-in-out ${on ? "bg-bit border-bit text-white" : ""}`}
+        className={`h-10 w-10 border font-bold border-white grid place-items-center rounded-full transition duration-500 ease-in-out ${on ? "bg-white border-white text-gray-600 animate-pulse" : ""}`}
       >
         {step}
       </div>
-      <div className={'hidden md:flex flex-col ${on ? "bg-bit border-bit text-white" : ""}'}>
-        <span className="font-oxy text-Light-gray">{type}</span>
-      </div>
+      <span className={`${on ? "animate-pulse text-gray-600 bg-white py-2 px-3 rounded-full" : ""}`}>{type}</span>
     </motion.div>
   );
 };
