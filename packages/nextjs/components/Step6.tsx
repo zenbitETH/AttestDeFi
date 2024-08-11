431f8a17e338a8ce093b258fd0334c962da7f936
Marcos Iglesias <markkos89@gmail.com>
2024-08-11 07:39:51 -0300

    feat: using regexp to validate decimals in the input

--- packages/nextjs/components/Step6.tsx
+++ packages/nextjs/components/Step6.tsx
@@ -44,9 +44,9 @@ const pageVariant: Variants = {
 export default function Step6() {
   const { disperseFormData } = useGlobalState();
   const [sum, setSum] = useState(0); //suma total
-  const [inputs, setInputs] = useState<any>({}); //  valores de los inputs
+  const [inputs, setInputs] = useState<{ [key: string]: number }>({}); //  valores de los inputs
   const [attestations, setAttestations] = useState([]); // manejo atestaciones
-  // const [distributionAmount] = useState<number>(0); // impresion cantidad distrib
+  const [distributionAmount, setDistributionAmount] = useState(0); // impresion cantidad distrib
 
   const GET_ATTESTERS = gql`
     query Attestations($schemaID: String!, $attesterAddress: String!) {
@@ -79,28 +79,38 @@ export default function Step6() {
   }, [data]);
 
   const { writeContractAsync } = useScaffoldWriteContract("Disperse");
+  // const { writeContractAsync: approveAsync } = useScaffoldWriteContract("approve");
 
   function getSum() {
     const values: any = Object.values(inputs); // obtiene el valor del array
     const sum = values.reduce((acc: number, c: number) => acc + c, 0); // suma el valor
     setSum(sum); // muestra valor
   }
+  console.log({ inputs });
 
   // Cantidad impresa imput
-  // function applyAmmout() {
-  //   const updatedInputs = Object.keys(inputs).reduce((acc: { [key: string]: number }, key) => {
-  //     acc[key] = distributionAmount; // asigno valor a cada imput
+  function applyAmmout() {
+    alert("weqwe");
+    const updatedInputs = Object.keys(inputs).reduce((acc: { [key: string]: number }, key) => {
+      console.log("applyAmmout k: " + key, { el: acc[key], distributionAmount });
+      acc[key] = distributionAmount; // asigno valor a cada imput
 
-  //     return acc;
-  //   }, {});
+      return acc;
+    }, {});
 
-  //   setInputs(updatedInputs);
-  // }
+    console.log({ inputs, updatedInputs });
+
+    setInputs(updatedInputs);
+  }
 
   //  cambio del imput
   function handleInput(e: any) {
     const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: +value });
+    const reg = new RegExp(/^\d*\.?\d*$/);
+    if (reg.test(value)) {
+      console.log("xd ", { value });
+      setInputs({ ...inputs, [name]: value });
+    }
   }
 
   // elimina fila
@@ -124,8 +134,29 @@ export default function Step6() {
 
     if (disperseFormData.typeOfReward !== "") {
       if (disperseFormData.typeOfReward === "custom" && disperseFormData.erc20address !== "") {
+        alert("disperseToken. TODO: increaseAllowance()");
+        const recipientsAddresses = attestations.map((att: any) => att.recipient);
+
+        let valuesToDisperse: any = Object.values(inputs); //.map((val: string) => parseEther(val));
+        valuesToDisperse = valuesToDisperse.map((value: any) => parseEther(value.toString(), "gwei")); // all the token rewards ammounts in bigint[]
+
+        await writeContractAsync(
+          {
+            functionName: "disperseToken",
+            args: ["0x6c8b35dC626Eb7906412f2e3e4F5254e28834a05", recipientsAddresses, valuesToDisperse],
+          },
+          {
+            onSuccess: (data, variables, context) => {
+              console.log("Diserse onSuccess ", { data, variables, context });
+              alert("Diserse success");
+            },
+            onError: (error: any) => {
+              console.log("disperToken onError! ", { error });
+            },
+          },
+        );
       } else if (disperseFormData.typeOfReward === "ETH") {
-        const recipientsAddresses = data.attestations.map((att: any) => att.recipient);
+        const recipientsAddresses = attestations.map((att: any) => att.recipient);
 
         let valuesToDisperse: any = Object.values(inputs); //.map((val: string) => parseEther(val));
         valuesToDisperse = valuesToDisperse.map((value: any) => parseEther(value.toString(), "gwei")); // all the token rewards ammounts in bigint[]
@@ -148,6 +179,9 @@ export default function Step6() {
               console.log("Diserse onSuccess ", { data, variables, context });
               alert("Diserse success");
             },
+            onError: (error: any) => {
+              console.log("disperEther onError! ", { error });
+            },
           },
         );
       }
@@ -164,6 +198,24 @@ export default function Step6() {
         className="flex flex-col mb-20 rounded-md z-30 relative bottom-24 text-[14px] md:bottom-0 md:p-0 h-full"
       >
         <Title title="Verify & Send 🚀">Check the addresses and define the reward amount before send it.</Title>
+        <div className="flex items-center mt-4 md:mt-0 space-x-2">
+          <input
+            type="number"
+            value={distributionAmount}
+            onChange={e => setDistributionAmount(+e.target.value)}
+            placeholder="Distribution amount"
+            className=" px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm w-full md:w-32"
+          />
+          <button
+            type="button"
+            onClick={() => applyAmmout()}
+            className="text-gray-500 text-lg font-bold hover:text-gray-700 p-2 rounded-full border border-gray-300 flex items-center justify-center mt-2 md:mt-0 md:ml-2"
+            title="Apply distribution amount"
+            style={{ width: "40px", height: "40px" }}
+          >
+            +
+          </button>
+        </div>
         <div className="w-full flex flex-col space-y-4 md:space-y-3 bg-Alabaster rounded-xl overflow-y-auto">
           <table className="table">
             <thead>
@@ -183,7 +235,6 @@ export default function Step6() {
                   </td>
                   <td>
                     <input
-                      defaultValue={10}
                       type="text"
                       name={`ammount-${idx + 1}`} // id input
                       value={inputs[`ammount-${idx + 1}`] || ""} // valor del input
